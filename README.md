b00261e5753b9f0bdd15b1dbac1d32ff8838537a
Anand Thakker <vestibule@anandthakker.net>
2015-09-21 19:41:11 -0400

    Basic docs

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # oam-catalog-grid
+
 Generate a vector tile grid from the OAM catalog
+
+## Install
+
+Install [tippecanoe](https://github.com/mapbox/tippecanoe).
+Run
+    npm install
+
+## Usage
+
+    curl api.openaerialmap.org | ./build_grid.sh oam-grid.mbtiles
+
+Builds a set of vector tiles `oam-grid.mbtiles`, which can be uploaded to
+Mapbox at https://mapbox.com/studio
--- build_grid.sh
+++ build_grid.sh
@@ -8,11 +8,12 @@ set -e
 
 # use tippecanoe to build the base layer from footprint features
 mkdir -p temp
+rm -f temp/footz.mbtiles
 cat - | node read-catalog.js | tippecanoe -l footprints -o temp/footz.mbtiles -z 10 -Z 10 -b 0 -ps
 
 # use tilelive-copy to copy over this tile set, because tippecanoe output
 # doesn't play perfectly with node-mbtiles
-tilelive-copy temp/footz.mbtiles $1
+node_modules/.bin/tilelive-copy temp/footz.mbtiles $1
 
 # build the grid
 node index.js $1
